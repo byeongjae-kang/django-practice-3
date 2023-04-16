e5d921bfbd7b49f106642038dd5710e1fba27fa1
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-16 03:29:13 -0700

    change location to one to many

--- meetups/models.py
+++ meetups/models.py
@@ -3,11 +3,19 @@ from django.db import models
 # Create your models here.
 
 
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.address}"
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
     image = models.ImageField(upload_to="images")
-    location = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
     slug = models.SlugField(unique=True)
 
     def __str__(self):
