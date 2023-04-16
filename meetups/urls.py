658603a7a36bfc6f935c5e4e7c6239d6efe2413b
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-16 01:53:03 -0700

    create templates and styles

--- meetups/urls.py
+++ meetups/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("meetups/", views.MeetupsView.as_view())]
+urlpatterns = [
+    path("meetups/", views.MeetupsView.as_view(), name="meetups"),
+    path("meetups/<slug:slug>", views.MeetupDetailView.as_view(), name="meetup_detail"),
+]
--- meetups/views.py
+++ meetups/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
+MEETUP_DATA = [
+    {"title": "title", "location": "location", "slug": "title"},
+    {"title": "title", "location": "location", "slug": "title"},
+    {"title": "title", "location": "location", "slug": "title"},
+]
+
 
 # Create your views here.
 class MeetupsView(View):
     def get(self, request):
-        return render(request, "meetups/meetups.html")
+        return render(request, "meetups/meetups.html", {"meetups": MEETUP_DATA})
+
+
+class MeetupDetailView(View):
+    def get(self, request, slug):
+        selected_meetup = {
+            "title": "A first meetup",
+            "description": "This is the first meetup",
+        }
+        return render(
+            request, "meetups/meetup_detail.html", {"meetup": selected_meetup}
+        )
