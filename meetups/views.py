82a8f3a1c1a0483d2b5f6d34f54b538fc8dd8a01
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-16 04:34:11 -0700

    add form to register paticipants

--- meetups/views.py
+++ meetups/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import View
+from django.http import HttpResponseRedirect
 from .models import Meetup
+from .forms import RegistrationForm
 
 
 # Create your views here.
@@ -13,6 +16,19 @@ class MeetupsView(View):
 class MeetupDetailView(View):
     def get(self, request, slug):
         selected_meetup = Meetup.objects.get(slug=slug)
+        registration_form = RegistrationForm()
         return render(
-            request, "meetups/meetup_detail.html", {"meetup": selected_meetup}
+            request,
+            "meetups/meetup_detail.html",
+            {"meetup": selected_meetup, "form": registration_form},
         )
+
+    def post(self, request, slug):
+        registration_form = RegistrationForm(request.POST)
+        selected_meetup = Meetup.objects.get(slug=slug)
+        print(registration_form)
+        if registration_form.is_valid():
+            participant = registration_form.save()
+            selected_meetup.participant.add(participant)
+
+        return HttpResponseRedirect(reverse("meetup_detail", args=[slug]))
