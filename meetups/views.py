f02adeb143cb0d5cc9d6cd4b53312d0d5050d372
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-15 22:23:11 -0700

    do basic setup to create meetups

--- meetups/views.py
+++ meetups/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
 
 # Create your views here.
+class MeetupsView(View):
+    def get(self, request):
+        return render(request, "meetups/meetups.html")
