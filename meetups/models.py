3bd92c7713fe1d4d7ad72e114b2df757ef69d2a1
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-16 01:53:22 -0700

    create model

--- meetups/models.py
+++ meetups/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Meetup(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+    location = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.location}"
+
+    def save():
+        super().save()
