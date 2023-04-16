226674ca77c4d1684012384aee95ce47c391ad4d
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-16 03:57:02 -0700

    add participant model

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Meetup, Location
+from .models import Meetup, Location, Participant
 
 # Register your models here.
 
@@ -13,3 +13,4 @@ class MeetupAdmin(admin.ModelAdmin):
 
 admin.site.register(Meetup, MeetupAdmin)
 admin.site.register(Location)
+admin.site.register(Participant)
--- meetups/models.py
+++ meetups/models.py
@@ -11,12 +11,22 @@ class Location(models.Model):
         return f"{self.name} - {self.address}"
 
 
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self) -> str:
+        return self.email
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
     image = models.ImageField(upload_to="images")
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
     slug = models.SlugField(unique=True)
+    organizer_email = models.EmailField()
+    date = models.DateField(null=True)
+    participant = models.ManyToManyField(Participant, blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} - {self.location}"
